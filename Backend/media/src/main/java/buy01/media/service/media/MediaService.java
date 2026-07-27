ae1014640a1a22ef1b9474bd591e18076ac182e6
aouchcha <ouchchatea@gmail.com>
2026-07-27 17:51:13 +0100

    tdhethsetghkgyufre

--- Backend/media/src/main/java/buy01/media/service/media/MediaService.java
+++ Backend/media/src/main/java/buy01/media/service/media/MediaService.java
@@ -142,8 +142,11 @@ public class MediaService {
         MultipartFile[] images = request.getNewImages();
 
         if (images != null && images.length > 0) {
+            System.out.println("=============================================\n"+request.getProductId());
+            List<MediaEntity> existingMedia = mediaRepository.findByProductIdAndType(request.getProductId(), "Product");
+            System.out.println("=============================================\n"+existingMedia.size());
+            System.out.println("=============================================\n"+images.length);
 
-            List<MediaEntity> existingMedia = mediaRepository.findByProductId(request.getProductId());
             if (existingMedia.size() + images.length > 3) {
                 throw new MyBadRequest("You can upload a maximum of 3 images for a product.");
             }
