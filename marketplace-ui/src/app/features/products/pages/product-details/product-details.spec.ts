a08761bb0ef291b5003361ccc76a76d1a0d8c32d
yakhaldy <yahyakhaldy9@gmail.com>
2026-08-01 13:38:26 +0100

    fix erros in unit test

--- marketplace-ui/src/app/features/products/pages/product-details/product-details.spec.ts
+++ marketplace-ui/src/app/features/products/pages/product-details/product-details.spec.ts
@@ -1,4 +1,5 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { provideRouter } from '@angular/router';
 
 import { ProductDetails } from './product-details';
 
@@ -9,6 +10,9 @@ describe('ProductDetails', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [ProductDetails],
+      providers: [
+        provideRouter([])
+      ]
     }).compileComponents();
 
     fixture = TestBed.createComponent(ProductDetails);
