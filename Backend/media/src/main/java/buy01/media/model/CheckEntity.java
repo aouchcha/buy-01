fac0d7d5d931ae80c825bd81da039a986216dd7c
yakhaldy <yahyakhaldy1@gmail.com>
2026-09-09 18:18:15 +0100

    refactor(validation): replace UUID annotation with Pattern for ID validation in CheckEntity, MediaEntity, and UserEntity

--- Backend/media/src/main/java/buy01/media/model/CheckEntity.java
+++ Backend/media/src/main/java/buy01/media/model/CheckEntity.java
@@ -1,6 +1,7 @@
 package buy01.media.model;
 
-import org.hibernate.validator.constraints.UUID;
+import javax.validation.constraints.Pattern;
+
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import lombok.Data;
@@ -8,7 +9,7 @@ import lombok.Data;
 @Data
 @Document(collection = "check")
 public class CheckEntity {
-    @UUID
+    @Pattern(regexp = "^[0-9a-fA-F]{8}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{12}$")
     private String id;
     private String ownerId;
     private String productId;
--- Backend/user/src/main/java/buy01/user/model/UserEntity.java
+++ Backend/user/src/main/java/buy01/user/model/UserEntity.java
@@ -1,6 +1,7 @@
 package buy01.user.model;
 
-import org.hibernate.validator.constraints.UUID;
+import javax.validation.constraints.Pattern;
+
 import org.springframework.data.mongodb.core.index.Indexed;
 import org.springframework.data.mongodb.core.mapping.Document;
 
@@ -9,7 +10,7 @@ import lombok.Data;
 @Data
 @Document(collection = "users")
 public class UserEntity {
-    @UUID
+    @Pattern(regexp = "^[0-9a-fA-F]{8}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{12}$")
     private String id;
 
     private String firstName;
